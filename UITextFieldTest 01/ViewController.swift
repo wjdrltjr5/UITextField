f81907c45d4893b5f422c4ff00c65660ccc32d5f
dit08 <dit@imac-3.local>
2019-04-03 16:55:24 +0900

    a

--- UITextFieldTest 01/ViewController.swift
+++ UITextFieldTest 01/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var myTextField: UITextField!
+    
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Delegate class Connection
+        myTextField.delegate = self
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        let inString = myTextField.text! + "Coding"
+        myLabel.text = inString
+        myTextField.text = ""
+        myTextField.resignFirstResponder() //키패드 권한을 박탈
+        
+    }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        myTextField.resignFirstResponder()
+        return true
+    }
+    public func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        myTextField.backgroundColor = UIColor.yellow
+        return true
+    }
 }
 
